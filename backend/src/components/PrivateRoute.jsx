94df14c90be9573b858d82ca390289db37a1b669
zsasz0 <88143714+zsasz0@users.noreply.github.com>
2025-12-17 11:43:20 +0100

    Delete backend directory

--- backend/src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem("authToken"); // Check if token exists
-  return isAuthenticated ? children : <Navigate to="/" />;
-};
-
-export default PrivateRoute;
